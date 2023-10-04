1c99c10b088a0b92ec84acb40b2b018210fb4c9b
NguyenTanLap <lapnguyen200319@gmail.com>
2023-10-04 08:43:27 +0700

    xu ly su kien

---
+++ XuLySuKien1/app/src/main/java/com/nguyentanlap/xulysukien1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.nguyentanlap.xulysukien1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+    //ten DN = 63CLC1 pass= 123
+    public void Login_Click(View v){
+
+    }
+}
