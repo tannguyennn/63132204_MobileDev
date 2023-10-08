0b31acaff67d694cff8f3c2ff3c3bfa856b7f168
NguyenTanLap <lapnguyen200319@gmail.com>
2023-10-08 15:54:45 +0700

    Xu ly su kien so hoc

--- SoHocDonGian/app/src/main/java/com/nguyentanlap/sohocdongian/MainActivity.java
+++ SoHocDonGian/app/src/main/java/com/nguyentanlap/sohocdongian/MainActivity.java
@@ -3,12 +3,53 @@ package com.nguyentanlap.sohocdongian;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText_A;
+    EditText editText_B;
+    TextView tvKQ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWidget();
+    }
+
+    public void getWidget() {
+        editText_A = (EditText) findViewById(R.id.editA);
+        editText_B = (EditText) findViewById(R.id.editB);
+        tvKQ = (TextView) findViewById(R.id.tvResult);
+    }
+
+    public void Xuly_Cong() {
+        int soA = Integer.parseInt(editText_A.getText().toString());
+        int soB = Integer.parseInt(editText_B.getText().toString());
+        int ketQua = soA + soB;
+        tvKQ.setText(String.valueOf(ketQua));
+    }
+
+    public void Xuly_Tru() {
+        int soA = Integer.parseInt(editText_A.getText().toString());
+        int soB = Integer.parseInt(editText_B.getText().toString());
+        int ketQua = soA - soB;
+        tvKQ.setText(String.valueOf(ketQua));
+    }
+
+    public void Xuly_Nhan() {
+        int soA = Integer.parseInt(editText_A.getText().toString());
+        int soB = Integer.parseInt(editText_B.getText().toString());
+        int ketQua = soA * soB;
+        tvKQ.setText(String.valueOf(ketQua));
+    }
+
+    public void Xuly_Chia() {
+        int soA = Integer.parseInt(editText_A.getText().toString());
+        int soB = Integer.parseInt(editText_B.getText().toString());
+        int ketQua = soA / soB;
+        tvKQ.setText(String.valueOf(ketQua));
     }
 }
