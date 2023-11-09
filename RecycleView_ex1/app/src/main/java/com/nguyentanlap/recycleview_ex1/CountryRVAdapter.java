e395c13eda25caab4ab274977c0eb49612e6c3c7
NguyenTanLap <135099342+tannguyennn@users.noreply.github.com>
2023-11-09 14:48:03 +0700

    upload RecycleView

--- RecycleView_ex1/app/src/main/java/com/nguyentanlap/recycleview_ex1/CountryRVAdapter.java
+++ RecycleView_ex1/app/src/main/java/com/nguyentanlap/recycleview_ex1/CountryRVAdapter.java
@@ -1,10 +1,13 @@
 package com.nguyentanlap.recycleview_ex1;
 
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -36,7 +39,11 @@ public class CountryRVAdapter extends RecyclerView.Adapter {
 
         @Override
         public void onClick(View view) {
-            int clickedPosition
+            int clickedPosition = getAdapterPosition();
+
+            Country country = lstData.get(clickedPosition);
+
+            Toast.makeText(view.getContext(), "You Chose" + country.getCountryName(), Toast.LENGTH_SHORT).show();
 
         }
     }
@@ -45,17 +52,32 @@ public class CountryRVAdapter extends RecyclerView.Adapter {
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.country_item_layout, parent, false);
+        CountryItemViewHolder viewHolder = new CountryItemViewHolder(v);
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        CountryItemViewHolder viewHolder = (CountryItemViewHolder) holder;
+        viewHolder.position = position;
+        Country country = lstData.get(position);
 
+        ((CountryItemViewHolder) holder).countryNameV.setText(country.getCountryName());
+        ((CountryItemViewHolder) holder).populationnV.setText(country.getPopulation());
+        ((CountryItemViewHolder) holder).flagV.setImageResource(getMipMapResID(holder.itemView,country.getCountryFlag()));
+
+    }
+
+    public int getMipMapResID(View itemView, String mipmapName)
+    {
+        String name = itemView.getContext().getPackageName();
+        return itemView.getResources().getIdentifier(mipmapName,"mipmap",name);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return lstData.size();
     }
 
 
--- RecycleView_ex1/app/src/main/java/com/nguyentanlap/recycleview_ex1/MainActivity.java
+++ RecycleView_ex1/app/src/main/java/com/nguyentanlap/recycleview_ex1/MainActivity.java
@@ -1,14 +1,42 @@
 package com.nguyentanlap.recycleview_ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    CountryRVAdapter countryRVAdapter;
+    ArrayList<Country> ds = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Country qg1 = new Country("Vietnam","vn",80);
+        Country qg2 = new Country("United State","us",68);
+        Country qg3 = new Country("Russia","ru",120);
+        ds.add(qg1);
+        ds.add(qg2);
+        ds.add(qg3);
+
+        RecyclerView recyclerView =(RecyclerView) findViewById(R.id.myrecycleView);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        itemDecoration.setDrawable(ContextCompat.getDrawable(this,R.drawable.divider));
+        recyclerView.addItemDecoration(itemDecoration);
+        countryRVAdapter= new CountryRVAdapter(ds);
+        recyclerView.setAdapter(countryRVAdapter);
+        countryRVAdapter.notifyDataSetChanged();
+
     }
 }
